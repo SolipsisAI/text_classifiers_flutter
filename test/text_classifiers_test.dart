34435ba29ae62fc0215a512a89fda5d7c6cbd964
AJ <bitjockey@jackpoint.network>
2023-01-30 18:44:23 -0500

    Generate mocks

--- test/text_classifiers_test.dart
+++ test/text_classifiers_test.dart
@@ -1,14 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ml_linalg/vector.dart';
+import 'package:mockito/annotations.dart';
+import 'package:tflite_flutter/tflite_flutter.dart' as tflite_flutter;
 
 import 'package:text_classifiers/text_classifiers.dart';
-import 'package:text_classifiers/src/util.dart' as util;
 
+import 'text_classifiers_test.mocks.dart';
+
+@GenerateMocks([tflite_flutter.Interpreter])
 void main() {
-  group('util', (() {
-    test('softmax', () async {
-      final expected = Vector.fromList([1, 2, 3]);
-      expect(util.softmax([1, 1, 1]), expected);
-    });
-  }));
+  group('classifier io', (() {}));
 }
