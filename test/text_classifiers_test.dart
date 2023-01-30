15ccb16a19554dd42b73ee9c0e301a0dd2dcf2ba
AJ <bitjockey@jackpoint.network>
2023-01-30 18:41:37 -0500

    Set up tests

--- test/text_classifiers_test.dart
+++ test/text_classifiers_test.dart
@@ -1,5 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:ml_linalg/vector.dart';
 
 import 'package:text_classifiers/text_classifiers.dart';
+import 'package:text_classifiers/src/util.dart' as util;
 
-void main() {}
+void main() {
+  group('util', (() {
+    test('softmax', () async {
+      final expected = Vector.fromList([1, 2, 3]);
+      expect(util.softmax([1, 1, 1]), expected);
+    });
+  }));
+}
