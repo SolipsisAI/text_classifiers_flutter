f1e866cd4d8529c093e3fddc2ee666d4729a39aa
AJ <bitjockey@jackpoint.network>
2023-01-30 20:26:29 -0500

    Add download file function

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 import 'package:ml_linalg/linalg.dart';
 
@@ -22,4 +24,29 @@ int argmax(Vector input) {
     }
   }
   return index;
+}
+
+void downloadFile(
+    String? url, String? filename, String? type, String? downloaddir) async {
+  String filepath = '$downloaddir/$filename';
+
+  debugPrint('[DOWNLOAD] $url -> $filepath');
+
+  if (File(filepath).existsSync()) {
+    debugPrint('[SKIP] Exists');
+    return;
+  }
+
+  final client = http.Client();
+  final response = await client.get(Uri.parse(url!));
+
+  File file = File(filepath);
+
+  if (type == 'bytes') {
+    await file.writeAsBytes(response.bodyBytes);
+  } else {
+    await file.writeAsString(response.body);
+  }
+
+  debugPrint("[SUCCESS] Downloaded $filepath");
 }
